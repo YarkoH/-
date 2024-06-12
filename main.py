5ff80d175ede22f07551354bd2056d4a533eaa6c
YarkoH <halavski@gmail.com>
2024-06-12 14:42:16 +0300

    12.06 знакомство сперменными

---
+++ main.py
@@ -0,0 +1,22 @@
+a = "будем учиться"
+print(len(a))                         # Задача "Длина слова"
+
+first =22
+second = 1
+summa = first + second
+diff = (first - second)
+print(diff, summa)                    # Cуммы разность
+
+third = 19
+mean = (first + second + third)/3
+print(mean)                           # Среднее арифметическое
+
+first_string = "Вторник"
+second_string = "Понедельник, "
+print(second_string + first_string)   # Простые строчки
+
+a = 2
+b = 4
+c = 8
+f = ((a * b) + (a * c))
+print((f ** 3) / 2)                   # Cложная фрмула
